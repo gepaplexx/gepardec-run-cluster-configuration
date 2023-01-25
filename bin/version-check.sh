b01e8db6fc309a1db14888b5c5fead19d5db5a8d
fhochleitner <felix.hochleitner@outlook.com>
2023-01-25 16:37:09 +0100

    GPX-571 gepardec-run-cluster-configuration konfigurieren sodass Dependency-Bot veraltete Helm Cha...
    added renovate bot.
    added github action to periodically run and generate new template files
    
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

---
+++ bin/version-check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PRG=`basename $0`
+
+DIR=$(dirname $0)
+HOME=${DIR-.}/..
+
+ARGO_APPLICATIONS="cluster-applications cluster-setup-checkpoint initial-cluster-setup"
+CLUSTERS="play steppe"
+
+for f in $(find $HOME -name Chart.yaml); do
+    chart=$(dirname $f)
+    for CLUSTER in ${CLUSTERS}; do
+      echo $chart
+      helm template ${chart} -s templates/02-argo-application.yaml --values ${chart}/values/values-${CLUSTER}.yaml > ${HOME}/versions/$(basename $chart)-${CLUSTER}.yaml
+    done
+done
